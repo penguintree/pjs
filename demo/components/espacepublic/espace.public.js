fd84f2058f87c04705a7ee51ea467d6aa6fb3c46
penguintree <pmalo@protonmail.com>
2017-05-21 15:34:36 -0400

    librairy startup
    a

---
+++ demo/components/espacepublic/espace.public.js
@@ -0,0 +1,18 @@
+(function(lib){
+   var def = {
+     template: {
+        url: '/components/espacepublic/espace.public.html'
+     },
+     methods: {
+     },
+     //model: {
+     //   description: bar,
+     //   reste: 8
+     //},
+     events: {
+        
+     }        
+   }
+   
+   window.espacePublic = lib.createComponent(def);
+})(window.pjs);
