e19e7621753d435f1a57c5be727e8a60f29bac63
penguintree <penguintree@users.noreply.github.com>
2017-05-27 21:36:03 -0400

    two-way binding

---
+++ demo/components/inputsDemo/inputs.demo.js
@@ -0,0 +1,14 @@
+(function(){
+   var def = {
+     template: {
+        url: '/components/inputsDemo/inputs.demo.html'
+     },
+     methods: {
+     },
+     events: {
+
+     }
+   }
+
+   window.inputDemo = pjs.createComponent(def);
+})(pjs);
