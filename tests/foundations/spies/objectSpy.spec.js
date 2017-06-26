2641b8614699378443b825d1b9c743bcee071518
penguintree <penguintree@users.noreply.github.com>
2017-06-25 23:17:23 -0400

    Added objectSpy tests, array.observable.js

--- tests/foundations/spies/objectSpy.spec.js
+++ tests/foundations/spies/objectSpy.spec.js
@@ -1,4 +1,6 @@
 describe('objectSpy.js', function(){
+   'use strict';
+   
    var objectSpy = window.pjs.spy.objectSpy
    var tick = new window.mocks.Tick();
 
@@ -90,11 +92,9 @@ describe('objectSpy.js', function(){
    describe('register', function(){
       it('simple prop - handler run on change', function(){
          var model = { prop: 'potato' };
-
          var spy = objectSpy(model, tick);
 
          var handler = jasmine.createSpy('spyHandler');
-
          spy.register('prop', handler);
 
          model.prop = 'soup';
@@ -106,11 +106,9 @@ describe('objectSpy.js', function(){
 
       it('deep prop -handler run on change', function(){
          var model = { deep: { prop: 'potato' } };
-
          var spy = objectSpy(model, tick);
 
          var handler = jasmine.createSpy('spyHandler');
-
          spy.register('deep.prop', handler);
 
          model.deep.prop = 'soup';
@@ -122,12 +120,10 @@ describe('objectSpy.js', function(){
 
       it('multiple handler - only good handler called', function(){
          var model = { prop: 'potato', deep: { prop: 'soup' } };
-
          var spy = objectSpy(model, tick);
 
          var handlerProp = jasmine.createSpy('spyHandlerA');
          var handlerDeep = jasmine.createSpy('spyHandlerB');
-
          spy.register('prop', handlerProp);
          spy.register('deep.prop', handlerDeep);
 
@@ -157,11 +153,9 @@ describe('objectSpy.js', function(){
 
       it('successive tick trigger - handler run once', function(){
          var model = { prop: 'potato' };
-
          var spy = objectSpy(model, tick);
 
          var handler = jasmine.createSpy('spyHandler');
-
          spy.register('prop', handler);
 
          model.prop = 'soup';
@@ -178,12 +172,10 @@ describe('objectSpy.js', function(){
 
       it('multiple handlers on same prop - every handlers triggered', function(){
          var model = { prop: 'potato' };
-
          var spy = objectSpy(model, tick);
 
          var handlerA = jasmine.createSpy('spyHandlerA');
          var handlerB = jasmine.createSpy('spyHandlerB');
-
          spy.register('prop', handlerA);
          spy.register('prop', handlerB);
 
@@ -197,16 +189,50 @@ describe('objectSpy.js', function(){
          expect(handlerB.calls.count()).toBe(1);
       });
 
-      xit('mutliple handlers on different props - every handlers triggered', function(){
+      it('mutliple handlers on different props - every handlers triggered', function(){
+         var model = { a: 'a', b: 'b' };
+         var spy = objectSpy(model, tick);
+
+         var handlerA = jasmine.createSpy('spyHandlerA');
+         var handlerB = jasmine.createSpy('spyHandlerB');
+         spy.register('a', handlerA);
+         spy.register('b', handlerB);
+
+         model.a = 'x';
+         tick.$trigger()
+
+         expect(handlerA).toHaveBeenCalledWith('a', 'x');
+         expect(handlerA.calls.count()).toBe(1);
+         expect(handlerB.calls.count()).toBe(0);
+
+         handlerA.calls.reset();
+         handlerB.calls.reset();
+
+         model.b = 'y';
+         tick.$trigger();
+
+         expect(handlerB).toHaveBeenCalledWith('b', 'y');
+         expect(handlerB.calls.count()).toBe(1);
+         expect(handlerA.calls.count()).toBe(0);
+
+         handlerA.calls.reset();
+         handlerB.calls.reset();
+
+         model.a = 'z';
+         model.b = 'z';
+         tick.$trigger();
+
+         expect(handlerA).toHaveBeenCalledWith('x', 'z');
+         expect(handlerB).toHaveBeenCalledWith('y', 'z');
+         expect(handlerA.calls.count()).toBe(1);
+         expect(handlerB.calls.count()).toBe(1);
       });
 
       it('successive affectation between triggers - handler called once', function(){
          var model = { prop: 'potato' };
-
          var spy = objectSpy(model, tick);
 
          var handler = jasmine.createSpy('spyHandler');
-
          spy.register('prop', handler);
 
          model.prop = 'soup';
@@ -219,16 +245,30 @@ describe('objectSpy.js', function(){
          expect(handler.calls.count()).toBe(1);
       });
 
-      xit('multiple spies on same object - every handlers triggers', function(){
+      it('multiple spies on same object - every handlers triggers', function(){
+         var model = { prop: 'potato' };
+         var spyA = objectSpy(model, tick);
+         var spyB = objectSpy(model, tick);
+
+         var handlerA = jasmine.createSpy('handlerA');
+         var handlerB = jasmine.createSpy('handlerB');
+         spyA.register('prop', handlerA);
+         spyB.register('prop', handlerB);
+
+         model.prop = 'soup';
+         tick.$trigger();
+
+         expect(handlerA).toHaveBeenCalledWith('potato', 'soup');
+         expect(handlerA.calls.count()).toBe(1);
+         expect(handlerB).toHaveBeenCalledWith('potato', 'soup');
+         expect(handlerB.calls.count()).toBe(1);
       });
 
       it('simple prop - setValue trigger event', function(){
          var model = { prop: 'potato' };
-
          var spy = objectSpy(model, tick);
 
          var handler = jasmine.createSpy('spyHandler');
-
          spy.register('prop', handler);
 
          spy.setValue('prop', 'soup');
@@ -241,11 +281,9 @@ describe('objectSpy.js', function(){
 
       it('deep prop - setValue trigger event', function(){
          var model = { deep: { prop: 'potato' } };
-
          var spy = objectSpy(model, tick);
 
          var handler = jasmine.createSpy('spyHandler');
-
          spy.register('deep.prop', handler);
 
          spy.setValue('deep.prop', 'soup');
@@ -256,16 +294,70 @@ describe('objectSpy.js', function(){
          expect(handler.calls.count()).toBe(1);
       });
 
-      xit('array prop - add item - event triggered', function(){
+      it('object prop - reference changed - event triggered', function(){
+         var model = { prop: { a: 1 } };
+         var spy = objectSpy(model, tick);
+
+         var handler = jasmine.createSpy('spyHandler');
+         spy.register('prop', handler);
+
+         model.prop = { x: 2 };
+         tick.$trigger();
+
+         expect(handler).toHaveBeenCalledWith({a: 1}, {x: 2});
+         expect(handler.calls.count()).toBe(1);
       });
 
-      xit('array prop - remove item - event triggered', function(){
+      xit('object prop - reference changed - can still spy on childs', function(){
+      });
+
+      xit('array prop - push item - event triggered', function(){
+         var model = { prop: [1, 2, 3] };
+         var spy = objectSpy(model, tick);
+
+         var handler = jasmine.createSpy('spyHandler');
+         spy.register('prop', handler);
+
+         model.prop.push(4);
+         tick.$trigger();
+
+         expect(handler).toHaveBeenCalledWith([1, 2, 3], [1, 2, 3, 4]);
+         expect(handler.calls.count()).toBe(1);
+      });
+
+      xit('array prop - pop item - event triggered', function(){
+         var model = { prop: [1, 2, 3] };
+         var spy = objectSpy(model, tick);
+
+         var handler = jasmine.createSpy('spyHandler');
+         spy.register('prop', handler);
+
+         var pop = model.prop.pop();
+         expect(pop).toBe(3);
+         tick.$trigger();
+
+         expect(handler).toHaveBeenCalledWith([1, 2, 3], [1, 2]);
+         expect(handler.calls.count()).toBe(1);
+      });
+
+      xit('array prop - splice array - event triggered', function(){
       });
 
       xit('array prop - set item - event triggered', function(){
       });
 
-      xit('array prop - replace array - event triggered', function(){
+      it('array prop - replace array - event triggered', function(){
+         var model = { prop: [1, 2, 3] };
+         var spy = objectSpy(model, tick);
+
+         var handler = jasmine.createSpy('spyHandler');
+         spy.register('prop', handler);
+
+         model.prop = ['a', 'b', 'c'];
+         tick.$trigger();
+
+         expect(handler).toHaveBeenCalledWith([1, 2, 3], ['a', 'b', 'c']);
+         expect(handler.calls.count()).toBe(1);
       });
 
       xit('array model - add item - event triggered', function(){
@@ -278,6 +370,18 @@ describe('objectSpy.js', function(){
       });
 
       xit('array model - replace array - event triggered', function(){
+      });
+
+      xit('constructor - no model - throw', function(){
+         expect(function(){
+            objectSpy(undefined, tick);
+         }).toThrow('first parameter must be defined');
+      });
+
+      xit('constructor - no tick - throw', function(){
+         expect(function(){
+            obectSpy({ a: 2 });
+         }).toThrow('second paremeter must be a tick');
       });
    });
 
