b5eb6c8f34a9dfd1a385e7d35395fe96ba5ace3d
Pascal <penguintree@users.noreply.github.com>
2017-06-27 18:55:08 -0400

    Got back loader script. May be useful.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var serveStatic = require('serve-static');
 app = connect();
 app
    .use(serveStatic(__dirname + '/demo'))
-   .use(serveStatic(__dirname + '/lib/__dist'))
+   .use(serveStatic(__dirname + '/lib'))
    .use(serveStatic(__dirname + '/tests'))
    .listen(8080, function(){
        console.log('Server running on 8080...');
